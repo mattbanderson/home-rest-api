41dea2dfa9bcdffce9feb1b7271c915523971978
Matt Anderson <matt@mattbanderson.com>
2017-03-22 21:55:50 -0400

    Update IP addresses and add MAC addresses.

--- config/config.js
+++ config/config.js
@@ -3,32 +3,38 @@ var config = {};
 config.plugs = [
   {
       "name": "Family Room Lamp",
-      "host": "192.168.0.198",
+      "host": "192.168.0.197",
+      "mac": "38:2b:78:01:71:a6",
       "id": "ECO-780171A6"
   },
   {
       "name": "Living Room Lamp",
       "host": "192.168.0.199",
+      "mac": "38:2b:78:00:d6:6a",
       "id": "ECO-7800D66A"
   },
   {
       "name": "Family Room Lamp 2",
-      "host": "192.168.0.179",
+      "host": "192.168.0.196",
+      "mac": "38:2b:78:05:50:40",
       "id": "ECO-78055040"
   },
   {
       "name": "Dining Room Lamp",
-      "host": "192.168.0.165",
+      "host": "192.168.0.195",
+      "mac": "38:2b:78:03:2f:08",
       "id": "ECO-78032F08"
   },
   {
       "name": "Cabinet",
-      "host": "192.168.0.166",
+      "host": "192.168.0.194",
+      "mac": "38:2b:78:05:7f:e9",
       "id": "ECO-78057FE9"
   },
   {
       "name": "Bedroom Lamp",
-      "host": "192.168.0.176",
+      "host": "192.168.0.193",
+      "mac": "38:2b:78:02:cf:9b",
       "id": "ECO-7802CF9B"
   }
 ];
