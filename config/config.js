75cf2d172c4e3fa31bb4fb964aa9fc8dc373973b
Matt Anderson <matt@mattbanderson.com>
2017-04-10 22:38:55 -0400

    Update IP addresses.

--- config/config.js
+++ config/config.js
@@ -3,37 +3,37 @@ var config = {};
 config.plugs = [
   {
       "name": "Family Room Lamp",
-      "host": "192.168.0.197",
+      "host": "192.168.0.110",
       "mac": "38:2b:78:01:71:a6",
       "id": "ECO-780171A6"
   },
   {
       "name": "Living Room Lamp",
-      "host": "192.168.0.199",
+      "host": "192.168.0.111",
       "mac": "38:2b:78:00:d6:6a",
       "id": "ECO-7800D66A"
   },
   {
       "name": "Family Room Lamp 2",
-      "host": "192.168.0.196",
+      "host": "192.168.0.112",
       "mac": "38:2b:78:05:50:40",
       "id": "ECO-78055040"
   },
   {
       "name": "Dining Room Lamp",
-      "host": "192.168.0.195",
+      "host": "192.168.0.113",
       "mac": "38:2b:78:03:2f:08",
       "id": "ECO-78032F08"
   },
   {
       "name": "Cabinet",
-      "host": "192.168.0.194",
+      "host": "192.168.0.114",
       "mac": "38:2b:78:05:7f:e9",
       "id": "ECO-78057FE9"
   },
   {
       "name": "Bedroom Lamp",
-      "host": "192.168.0.193",
+      "host": "192.168.0.115",
       "mac": "38:2b:78:02:cf:9b",
       "id": "ECO-7802CF9B"
   }
